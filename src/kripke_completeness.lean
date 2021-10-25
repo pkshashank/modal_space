04fa012697c7ca0c620a3dee39e92107dfe9c3a0
Shashank Pathak <74153550+pathakshashankgit@users.noreply.github.com>
2021-10-25 16:59:57 +0530

    Add files via upload

---
+++ src/kripke_completeness.lean
@@ -0,0 +1,37 @@
+import kripke_semantics
+
+----------------------------------------------------------
+
+-- We define uniform substitution.
+@[simp]
+def subs (var_sub : ℕ → bmod_form) : bmod_form → bmod_form
+| (p n) := var_sub n
+| ⊥ := ⊥
+| (! φ) := ! (subs φ)
+| (φ ⋀ ψ) := (subs φ) ⋀ (subs ψ)
+| (◇ φ) := ◇ (subs φ)
+
+-- Next, we define a relation between modal formula, namely of being a substitution instance.
+
+def subs_inst : bmod_form → bmod_form → Prop := λ ψ φ, (∃ (v : ℕ → bmod_form), ψ = subs v φ)
+
+-- As an example, we see that ((□ p 1 ⋁ p 2) ⋀ (! p 3 ⇒ □ p 4)) ⋁ p 5 is a substitution instance of ((p 1 ⋀ p 2) ⋁ p 3).
+--For that we need to show the existence of an appropriate var_sub.
+
+example : subs_inst (((□ p 1 ⋁ p 2) ⋀ (! p 3 ⇒ □ p 4)) ⋁ p 5) ((p 1 ⋀ p 2) ⋁ p 3) :=
+begin
+  rw subs_inst,
+    let v : ℕ → bmod_form := λ n, match n with
+      | 1 := (□ p 1 ⋁ p 2)
+      | 2 := (! p 3 ⇒ □ p 4)
+      | 3 := p 5
+      | a := bmod_form.var a
+    end,
+
+  existsi v,
+  simp,
+
+  repeat {split},
+  
+
+end
